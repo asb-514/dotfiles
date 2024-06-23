70a12c0d29ef0cbbc33bdd7e18edda9de12039ea
asb-514 <shashankbabu2004@gmail.com>
2024-06-23 15:40:59 +0530

    Mac commit

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,5 +5,7 @@ config.font = wezterm.font ('CodeNewRoman Nerd Font', {weight = 'Bold'})
 config.font_size = 15
 --config.color_scheme = "Catppuccin Latte" -- or Macchiato, Frappe, Latte
 hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.1
+config.macos_window_background_blur = 15
 
 return config
