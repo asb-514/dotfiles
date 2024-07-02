605cba0fd7440614336bb8a71e859ae964a17661
ababu3 <ababu3@atlassian.com>
2024-07-02 15:24:33 +0530

    Office laptop

--- .config/nvim/lua/plugins/codium.lua
+++ .config/nvim/lua/plugins/codium.lua
@@ -1,7 +1,8 @@
 -- Remove the `use` here if you're using folke/lazy.nvim.
 return  {
 	'Exafunction/codeium.vim',
-	event = 'BufEnter',
+	--event = 'BufEnter',
+	lazy = true,
 	config = function ()
 		-- Change '<C-g>' here to any keycode you like.
 		vim.keymap.set('i', '<C-l>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
